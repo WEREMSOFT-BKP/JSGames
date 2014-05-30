4903311ad09aeecb4e7e2df5e6895e0e20bc46e5
werep002 <pablo.weremczuk@gmail.com>
2014-05-30 12:18:09 -0300

    updateo cordova a 3.5

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ C:\Proyectos\phonegap\JSGames\platforms\android\res\drawable-mdpi\icon.png \
 C:\Proyectos\phonegap\JSGames\platforms\android\res\drawable-xhdpi\icon.png \
 C:\Proyectos\phonegap\JSGames\platforms\android\res\values\strings.xml \
 C:\Proyectos\phonegap\JSGames\platforms\android\res\xml\config.xml \
+C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\res\drawable\icon.png \
+C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\Proyectos\phonegap\JSGames\platforms\android\ant-build\AndroidManifest.xml \
